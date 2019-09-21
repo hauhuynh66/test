c26628eb58b9190f46209a455cfdd7a34c8ff6a3
hauhuynh66 <hauhuynh66@gmail.com>
2019-09-21 23:57:06 +0700

    update

--- src/main/java/com/house/controller/SensorController.java
+++ src/main/java/com/house/controller/SensorController.java
@@ -3,24 +3,30 @@ package com.house.controller;
 import com.house.model.SensorValue;
 import com.house.repository.SensorValueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 
+@Controller
 @RequestMapping("/sensor")
 public class SensorController {
     @Autowired
     private SensorValueRepository sensorValueRepository;
 
     @PostMapping("/dht")
-    public void processTemperature(@RequestParam(name = "temp")double t,
+    @ResponseBody
+    public String processTemperature(@RequestParam(name = "temp")double t,
                                    @RequestParam(name = "humid")double humid){
         SensorValue value = new SensorValue();
         value.setTemp(t);
         value.setHumid(humid);
         value.setTime(new Date());
         sensorValueRepository.save(value);
+        return "Success";
+    }
+    @GetMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "OI BRUV";
     }
 }
